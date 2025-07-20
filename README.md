8fb7a11dbfd41fdbd151d6e4e956d55eda426fbc
tanvir2020 <tanviraut162009@gmail.com>
2025-07-20 14:23:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Minimum requirements for this project 
+1. LLM Models #groq, open ai, 
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Document_portal",
+    author="Tanvi Raut",
+    version="0.1",
+    packages=find_packages(),
+)
